be491bf6a3dedae640f6f5f69cbdd508486c5014
dojo1235 <dojo10295@gmail.com>
2025-10-16 19:09:18 +0100

    refactor(products): decouple Drizzle db mixed service into TypeORM service and repository pattern

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductsRepository } from './products.repository'
 import { ProductsService } from './products.service'
 import { AdminsProductsController } from './admins-products.controller'
 import { ProductsController } from './products.controller'
+import { Product } from 'src/products/entities/product.entity'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [
     AdminsProductsController,
     ProductsController,
-    ],
-  providers: [ProductsService],
+  ],
+  providers: [ProductsService, ProductsRepository],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
