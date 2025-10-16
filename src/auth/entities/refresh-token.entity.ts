1842722332a897f8fabb36837adebacb9c84ba97
dojo1235 <dojo10295@gmail.com>
2025-10-16 17:06:59 +0100

    feat(auth): create TypeORM refresh token  entity

---
+++ src/auth/entities/refresh-token.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, JoinColumn } from 'typeorm'
+import { User } from 'src/users/entities/user.entity'
+
+@Entity('refreshTokens')
+export class RefreshToken {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  userId: User
+
+  @Column({ type: 'varchar', length: 255 })
+  token: string
+
+  @Column({ type: 'tinyint', default: false })
+  revoked: boolean
+
+  @Column({ type: 'timestamp' })
+  expiresAt: Date
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date
+
+  @ManyToOne(() => User, { onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'revokedBy' })
+  revokedBy: User
+
+  @Column({ type: 'timestamp', nullable: true })
+  revokedAt: Date | null
+}
