e47daec95a6e3d4ae4fbe0ece25c5bbdbf7a083b
dojo1235 <dojo10295@gmail.com>
2025-10-16 19:28:04 +0100

    feat(products): add products list response DTO for swagger sample

---
+++ src/products/dto/products-list-response.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { ProductResponseDto } from './product-response.dto'
+import { MetaResponseDto } from 'src/common/dto/meta-response.dto'
+
+export class ProductsListResponseDto {
+  @ApiProperty({ description: 'List of products', type: [ProductResponseDto] })
+  products: ProductResponseDto[]
+
+  @ApiProperty({ description: 'Pagination metadata', type: MetaResponseDto })
+  meta: MetaResponseDto
+}
