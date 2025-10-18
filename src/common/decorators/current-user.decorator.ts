b11e1c00ccb17ef472250f621791fb09792cf5c1
dojo1235 <dojo10295@gmail.com>
2025-10-18 18:04:37 +0100

    refactor(products): type user via CurrentUser decorator in controllers

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,7 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
-export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest()
+type CurrentUserPayload = {
+  id: number
+  role: string
+}
 
-  return req.user
-})
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): CurrentUserPayload => {
+    const req = ctx.switchToHttp().getRequest()
+    return req.user as CurrentUserPayload
+  },
+)
