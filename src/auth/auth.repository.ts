3a3caa1b00c18235815521ce2c2bfc0c2b344376
dojo1235 <dojo10295@gmail.com>
2025-10-19 18:26:02 +0100

    chore(tsconfig): enable strict mode

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -10,23 +10,23 @@ export class AuthRepository {
     private readonly repository: Repository<RefreshToken>,
   ) {}
 
-  createRefreshToken(data) {
-    return this.repository.save(this.repository.create(data))
+  createRefreshToken(data: { userId: number; token: string; expiresAt: Date }) {
+    return await this.repository.save(this.repository.create(data))
   }
 
   findActiveTokensByUserId(userId) {
-    return this.repository.find({ where: { userId, revoked: false } })
+    return await this.repository.find({ where: { userId, revoked: false } })
   }
 
   revokeToken(refreshTokenId, data) {
-    return this.repository.update({ id: refreshTokenId, revoked: false }, data)
+    return await this.repository.update({ id: refreshTokenId, revoked: false }, data)
   }
 
   revokeAllTokensForUser(userId, data) {
-    return this.repository.update({ userId, revoked: false }, data)
+    return await this.repository.update({ userId, revoked: false }, data)
   }
 
   deleteTokensByUserId(userId) {
-    return this.repository.delete({ userId })
+    return await this.repository.delete({ userId })
   }
 }
