8ba51aea585e3d977097746b15d348c1322dff8a
dojo1235 <dojo10295@gmail.com>
2025-10-19 23:40:22 +0100

    fix(products): type Products service for strict typescript comparability

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common'
 import { ProductsRepository } from './products.repository'
 import { AppError, ErrorCode } from 'src/common/exceptions/app-error'
+import { CreateProductDto } from './dto/create-product.dto'
+import { FindProductsDto } from './dto/find-products.dto'
+import { Product } from './entities/product.entity'
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly productsRepository: ProductsRepository) {}
+
   // Create product (admin)
-  async createProduct(data, adminId) {
+  async createProduct(data: CreateProductDto, adminId: number) {
     const created = await this.productsRepository.createProduct({
       ...data,
       createdBy: adminId,
@@ -15,32 +19,32 @@ export class ProductsService {
   }
 
   // Find all products (admin)
-  async findAllProductsForAdmin(query) {
+  async findAllProductsForAdmin(query: FindProductsDto) {
     return await this.productsRepository.findAllProducts(query)
   }
 
   // Find all products (user)
-  async findAllProducts(query) {
+  async findAllProducts(query: FindProductsDto) {
     query.isDeleted = false
     return await this.productsRepository.findAllProducts(query)
   }
 
   // Find one product (admin)
-  async findOneProductForAdmin(productId) {
+  async findOneProductForAdmin(productId: number) {
     const product = await this.productsRepository.findProductById(productId)
     if (!product) throw new AppError(ErrorCode.NOT_FOUND, 'Product not found')
     return { product }
   }
 
   // Find one product (user)
-  async findOneProduct(productId) {
+  async findOneProduct(productId: number) {
     const product = await this.productsRepository.findProductById(productId)
     if (!product || product.isDeleted) throw new AppError(ErrorCode.NOT_FOUND, 'Product not found')
     return { product }
   }
 
   // Update product (admin)
-  async updateProduct(productId, data) {
+  async updateProduct(productId: number, data: Partial<Product>) {
     const existing = await this.productsRepository.findProductById(productId)
     if (!existing) throw new AppError(ErrorCode.NOT_FOUND, 'Product not found')
     await this.productsRepository.updateProduct(productId, data)
