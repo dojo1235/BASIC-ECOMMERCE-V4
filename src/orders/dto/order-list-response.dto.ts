36111fe69c7d7f698fb376ba56eff0e8f25f9e45
dojo1235 <dojo10295@gmail.com>
2025-10-20 17:46:14 +0100

    feat(orders): add order list response dto

---
+++ src/orders/dto/order-list-response.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { OrderResponseDto } from './order-response.dto'
+import { MetaResponseDto } from 'src/common/dto/meta-response.dto'
+
+export class OrdersListResponseDto {
+  @ApiProperty({ description: 'List of orders', type: [OrderResponseDto] })
+  orders!: OrderResponseDto[]
+
+  @ApiProperty({ description: 'Pagination metadata', type: MetaResponseDto })
+  meta!: MetaResponseDto
+}
