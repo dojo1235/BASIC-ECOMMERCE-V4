d5a0a0a196c9510f8fd0b45cdfac0e5f561713be
dojo1235 <dojo10295@gmail.com>
2025-10-21 18:02:10 +0100

    fix(auth): ensure proper serialization of nested user DTO in RegisterResponseDto

--- src/auth/dto/register-response.dto.ts
+++ src/auth/dto/register-response.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { Type } from 'class-transformer'
 import { UserResponseDto } from 'src/users/dto/user-response.dto'
 import { TokensResponseDto } from './tokens-response.dto'
 
 export class RegisterResponseDto {
+  @Type(() => UserResponseDto)
   @ApiProperty({ description: 'Registered user details', type: () => UserResponseDto })
   user: UserResponseDto
 
